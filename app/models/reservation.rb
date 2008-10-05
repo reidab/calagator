258e63ca718c28858747d57d417cd2993bf58221
Igal Koshevoy <igal@pragmaticraft.com>
2008-10-04 19:50:44 -0700

    Improved Reservation, added fixed statuses and validations.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,13 @@
 class Reservation < ActiveRecord::Base
+  # Associations
   belongs_to :user
   belongs_to :event
+
+  # Allowed status values
+  STATUSES = %w[yes no maybe].map(&:to_s)
+
+  # Validations
+  validates_inclusion_of :status, :in => STATUSES
+  validates_presence_of :user_id
+  validates_presence_of :event_id
 end
