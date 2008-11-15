777afc0241b3cbb42b6ee295781acfa5abef7d82
igal.koshevoy <igal.koshevoy@54c6411d-1744-0410-a9eb-b99409f43359>
2008-11-15 16:04:29 +0000

    Added MyEvent::INTERESTING_STATUSES and MyEvent::interesting.
    git-svn-id: https://calagator.googlecode.com/svn/branches/with_my_events@1280 54c6411d-1744-0410-a9eb-b99409f43359

--- app/models/my_event.rb
+++ app/models/my_event.rb
@@ -6,10 +6,16 @@ class MyEvent < ActiveRecord::Base
   # Allowed status values
   STATUSES = %w[yes maybe no]
 
+  # Statuses interpreted as "interesting" enough to list
+  INTERESTING_STATUSES = %w[yes maybe]
+
   # Validations
   validates_inclusion_of :status, :in => STATUSES
   validates_presence_of :user_id
   validates_presence_of :event_id
+
+  # Scopes
+  named_scope :interesting, :conditions => ['status in (?)', INTERESTING_STATUSES]
 
   # Return a human-readable status label for the given +status+.
   def self.status_label_for(status)
